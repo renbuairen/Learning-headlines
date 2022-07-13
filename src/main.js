a993b384412f78479a29dcec39be2797d5c1ba16
renbuairen <1664844124@qq.com>
2022-07-13 12:05:43 +0800

    完成项目初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 引入iconfount css
+import '@/assets/fonts/iconfont.css'
+// 引入flexble
+import 'amfe-flexible/index.min.js'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
